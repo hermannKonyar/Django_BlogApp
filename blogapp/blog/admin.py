63a90e1172d978dd095e557e8d7a0c2663a494c4
erman konyar <er.konyar@gmail.com>
2022-12-12 22:59:45 +0300

    OnUcuncu Commit

--- blogapp/blog/admin.py
+++ blogapp/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import BlogEntry,UserInfo,Videos,FoodBlog
 
 # Register your models here.
+
+
+class BolEntryAdmin(admin.ModelAdmin):
+    list_display=('title','isActive','isHome')
+    list_editable=('isActive','isHome')
+    search_fields=('title','description')
+
+
+admin.site.register(BlogEntry)
+admin.site.register(UserInfo)
+admin.site.register(Videos)
+admin.site.register(FoodBlog)
--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -1,38 +1,55 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 
 class BlogEntry(models.Model):
-    title=models.CharField(max_length=200)
-    image=models.CharField(max_length=200)
-    description=models.TextField()
-    entry=models.TextField()
-    isActive=models.BooleanField()
-    isHome=models.BooleanField()
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    description = models.TextField()
+    entry = models.TextField()
+    video=models.URLField(null=True)
+    slug=models.SlugField(null=True, blank=True,unique=True, db_index=True)
+    isActive = models.BooleanField()
+    isHome = models.BooleanField()
+    
+    def __str__(self):
+        return f'{self.title}'
+    
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        super().save(*args,**kwargs)
+
 
 class FoodBlog(models.Model):
-    title=models.CharField(max_length=200)
-    image=models.CharField(max_length=200)
-    video=models.CharField(max_length=200)
-    description=models.TextField()
-    entry=models.TextField()
-    isActive=models.BooleanField()
-    isHome=models.BooleanField()
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    description = models.TextField()
+    entry = models.TextField()
+    video=models.URLField(null=True)
+    slug = models.SlugField(null=True,blank=True ,unique=True, db_index=True)
+    isActive = models.BooleanField()
+    isHome = models.BooleanField()
+    
+    
+    def __str__(self):
+        return f'{self.title}'
+    
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        super().save(*args,**kwargs)
+
 
 class UserInfo(models.Model):
-    name=models.CharField(max_length=200)
-    image=models.CharField(max_length=200)
-    description =models.TextField()
-    isActive=models.BooleanField()
-    
-    
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    description = models.TextField()
+    isActive = models.BooleanField()
+
+
 class Videos(models.Model):
-    title=models.CharField(max_length=200)
-    name=models.CharField(max_length=200)
-    url=models.TextField()
-    imageUrl=models.TextField()
-    description=models.TextField()
-    isActive=models.BooleanField()
-    
-
-
+    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    url = models.TextField()
+    imageUrl = models.TextField()
+    description = models.TextField()
+    isActive = models.BooleanField()
